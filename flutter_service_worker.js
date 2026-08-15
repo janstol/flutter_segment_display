fe5926ca60eed845c43f0491f1c9314e0c287361
janstol <janstol@users.noreply.github.com>
2026-08-15 19:25:22 +0000

    deploy: edc559ab0295707a068c62933472ab64f03d8e1a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,8 +6,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"version.json": "c5edc99e768535869b588507940e0870",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "c4a41d019988373cd4ff7da50c4bd15c",
-"/": "c4a41d019988373cd4ff7da50c4bd15c",
+"index.html": "5fda3dee91805c6a2bddc43d9e1581c6",
+"/": "5fda3dee91805c6a2bddc43d9e1581c6",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "canvaskit/skwasm_heavy.js": "413f5b2b2d9345f37de148e2544f584f",
@@ -30,7 +30,7 @@ const RESOURCES = {"version.json": "c5edc99e768535869b588507940e0870",
 "assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "main.dart.js": "d18dbb4fc33cd4d9217532edaa3428fb",
-"flutter_bootstrap.js": "f4dcbd372e6cd2f32135655aa2f8afbc",
+"flutter_bootstrap.js": "011feb031abcbcd8be86a4a796ac1019",
 "manifest.json": "3c6874c6f997dbd23f8950a28accbd7a"};
 // The application shell files that are downloaded before a service worker can
 // start.
